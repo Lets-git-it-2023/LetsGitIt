e790e2f06fc84e48a6a30cab98b892492492dafd
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-06-08 23:16:42 +0900

    feat: add Team Member

--- src/main/resources/frontend/src/pages/projectBoard/ProjectBoard.tsx
+++ src/main/resources/frontend/src/pages/projectBoard/ProjectBoard.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
-import { AiFillSetting } from "react-icons/ai";
+import { AiFillHome, AiFillSetting } from "react-icons/ai";
 import GoBack from "../../components/GoBack";
 import CommitGraph from "./CommitGraph";
 import CommitLevel from "./CommitLevel";
 import ProjectChatBox from "./ProjectChatBox";
 import { ProjectDday } from "./ProjectDday";
 import Language from "./Language";
+import ProjectMembers from "../../components/Project/ProjectMembers";
+import { BsFillSendFill, BsGithub, BsArrowUpRight }from "react-icons/bs";
+import { BiLinkAlt } from "react-icons/bi";
+import { SiNotion } from "react-icons/si";
 
 const Wrapper = styled.div`
   display: grid;
@@ -33,9 +37,11 @@ const BackContainer = styled.div`
 
 const TeamContainer = styled.div`
   display: flex;
+  flex-direction: column;
   grid-area: team;
   max-width: 290px;
-  height: 740px;
+  height: 100%;
+  border-radius: 20px;
   background-color: var(--color-sub-2);
 `;
 
@@ -65,6 +71,7 @@ const TitleContainer = styled.div`
 
 const DdayContainer = styled.div`
   max-width: 400px;
+  height: 100%;
   grid-area: dday;
 `;
 
@@ -99,16 +106,178 @@ const CommitLevelContainer = styled.div`
   grid-area: level;
 `;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  width: 100%;
+`;
+
+const IconButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  background-color: var(--color-sub-2);
+  cursor: pointer;
+
+  svg{
+    width: 12px;
+    height: 11px;
+    fill: var(--color-sub-3);
+    :hover{
+      fill: var(--color-main-4);
+    }
+  }
+`;
+
+const URLButton = styled.button`
+  max-width: 120px;
+  max-height: 120px;
+  min-height: 65px;
+  min-width: 30px;
+  flex: 0 0 calc(30% - 20px);
+  padding: 25px;
+  border-radius: 10px;
+  color: var(--color-sub-3);
+  background-color: var(--color-sub-4);
+  font-size: 0.93rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.3));
+  white-space: pre-line;
+  position: relative;
+`;
+
+const ArrowUp = styled.div`
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  svg{
+    height: 24px;
+    width: 24px;
+    fill: var(--color-sub-1);
+  }
+`;
+
+const LinkContainer = styled.div`
+  position: absolute;
+  bottom: 10%;
+  left: 50%;
+  transform: translateX(-50%);
+  color: var(--color-sub-1);
+  font-size: 15px;
+  display: flex;
+  flex-direction: row;
+  white-space: nowrap;
+  svg{
+    width: auto;
+    height: 18px;
+    fill: var(--color-sub-1);
+    margin-right: 5px;
+  }
+`;
+
+const URLButtonContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 10px;
+  padding: 35px 10px;
+  box-sizing: border-box;
+`;
+
+const ProjectMemberContainer = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+`;
+
 export const ProjectBoard = () => {
+  const [isInitial , setIsInitial] = useState(false);
+  const [proejctLink, setProjectLinks] = useState(
+    {
+      github: "github.com",
+      notion: "notion.com",
+      link1: "link1",
+      link2: null
+    }
+    )
+  
   const onClickEditButton = () => {
     //go to edit page
   };
+  const teamMember = [
+    {
+      username: "user1",
+      image: "",
+      part: "안드로이드"
+    },{
+      username: "user2",
+      image: "",
+      part: "서버"
+    }
+  ]
+
   return (
     <Wrapper>
       <BackContainer>
         <GoBack />
       </BackContainer>
-      <TeamContainer></TeamContainer>
+      <TeamContainer>
+        <ProjectMemberContainer>
+        <ProjectMembers 
+        team={teamMember}
+        UserButton={<IconButton><AiFillHome/></IconButton>}
+        OthersButton={
+          <ButtonContainer>
+          <IconButton><AiFillHome/></IconButton>
+          <IconButton><BsFillSendFill/></IconButton>
+        </ButtonContainer>
+        }
+        />
+        </ProjectMemberContainer>
+        {isInitial ?
+        <URLButtonContainer>
+          <URLButton>
+            URL을{"\n"}추가하세요
+          </URLButton>
+
+          <URLButton>
+            URL을{"\n"}추가하세요
+          </URLButton>
+        </URLButtonContainer>
+      :
+      <URLButtonContainer>
+        <URLButton>
+         <ArrowUp><BsArrowUpRight/></ArrowUp>
+         <LinkContainer><BsGithub />깃허브</LinkContainer>
+        </URLButton>
+
+        <URLButton>
+        <ArrowUp><BsArrowUpRight /></ArrowUp>
+         <LinkContainer><SiNotion />팀 노션</LinkContainer>
+        </URLButton>
+
+        <URLButton>
+        <ArrowUp><BsArrowUpRight /></ArrowUp>
+         <LinkContainer><BiLinkAlt />참고 링크</LinkContainer>
+        </URLButton>
+        <URLButton>
+        <ArrowUp><BsArrowUpRight /></ArrowUp>
+         <LinkContainer><BiLinkAlt />참고 링크</LinkContainer>
+        </URLButton>
+    </URLButtonContainer>
+      }
+      </TeamContainer>
 
       <TitleContainer>
         커리어 SNS 기반 멘토링 플랫폼 프로젝트{" "}
