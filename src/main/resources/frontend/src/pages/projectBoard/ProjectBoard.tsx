773dad5b3a33372652cf9afc7c66ec76bb482aa9
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-06-10 21:17:12 +0900

    refactor: seperate project links component

--- src/main/resources/frontend/src/pages/projectBoard/ProjectBoard.tsx
+++ src/main/resources/frontend/src/pages/projectBoard/ProjectBoard.tsx
@@ -1,48 +1,80 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { AiFillHome, AiFillSetting } from "react-icons/ai";
 import GoBack from "../../components/GoBack";
 import CommitGraph from "./CommitGraph";
-import CommitLevel from "./CommitLevel";
+import { CommitLevel } from "./CommitLevel";
 import ProjectChatBox from "./ProjectChatBox";
-import { ProjectDday } from "./ProjectDday";
+import { MiniDday, ProjectDday } from "./ProjectDday";
 import Language from "./Language";
 import ProjectMembers from "../../components/Project/ProjectMembers";
-import { BsFillSendFill, BsGithub, BsArrowUpRight }from "react-icons/bs";
-import { BiLinkAlt } from "react-icons/bi";
-import { SiNotion } from "react-icons/si";
+import { BsFillSendFill } from "react-icons/bs";
+import ProjectLinks from "./ProjectLinks";
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
 const Wrapper = styled.div`
   display: grid;
   justify-content: center;
   align-items: center;
-  grid-template-columns: repeat(4, minmax(auto, max-content));
-  grid-template-rows: repeat(5, minmax(auto, max-content));
-  gap: 40px;
+  align-content: center;
+  grid-template-columns: repeat(4, max-content);
+  grid-template-rows: repeat(5, max-content);
+  gap: 30px;
   margin-bottom: 100px;
   width: 100%;
+  max-width: 1280px;
   grid-template-areas:
     "back back back back"
     "team title title title"
     "team dday dday lang"
     "team dday dday level"
     "team month today chat";
+  @media (max-width: 768px) {
+    padding: 1rem;
+    box-sizing: border-box;
+    row-gap: 20px;
+    column-gap: 15px;
+    grid-template-rows: repeat(auto, 1fr);
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+      "title title"
+      "team team"
+      "lang lang"
+      "dday level"
+      "month today"
+      "chat chat"
+      "link link";
+  }
 `;
 
 const BackContainer = styled.div`
   display: flex;
   width: 100%;
   grid-area: back;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const TeamContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   grid-area: team;
-  max-width: 290px;
+  //max-width: 290px;
   height: 100%;
   border-radius: 20px;
   background-color: var(--color-sub-2);
+  @media (max-width: 768px) {
+    width: 100%;
+    background-color: transparent;
+    grid-area: "";
+  }
 `;
 
 const TitleContainer = styled.div`
@@ -56,7 +88,7 @@ const TitleContainer = styled.div`
   border-radius: 14px;
   color: var(--color-sub-1);
   max-width: 950px;
-  height: 50px;
+  //height: 50px;
   padding: 12px 20px;
   font-size: 1.5rem;
   font-weight: 500;
@@ -67,43 +99,64 @@ const TitleContainer = styled.div`
     fill: var(--color-sub-1);
     cursor: pointer;
   }
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
 `;
 
 const DdayContainer = styled.div`
   max-width: 400px;
   height: 100%;
   grid-area: dday;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MiniDdayContainer = styled.div`
+  max-width: 400px;
+  height: 100%;
+  grid-area: dday;
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
+  }
 `;
 
 const LangContainer = styled.div`
   display: flex;
   grid-area: lang;
-  max-width: 510px;
-  height: 127px;
+  //max-width: 510px;
 `;
 
 const ChatContainer = styled.div`
-  max-width: 510px;
+  width: 100%;
   height: 330px;
   grid-area: chat;
 `;
 
 const CommitGraphContainer = styled.div`
-  max-width: 180px;
   height: 330px;
   grid-area: month;
 `;
 
 const TodayCommitContainer = styled.div`
-  max-width: 180px;
+  //max-width: 180px;
   height: 330px;
   grid-area: today;
 `;
 
 const CommitLevelContainer = styled.div`
   max-width: 510px;
-  height: 105px;
+  height: 100%;
+  width: 100%;
   grid-area: level;
+  box-sizing: border-box;
+  @media (max-width: 768px) {
+    padding: 0;
+    background-color: var(--color-sub-2);
+    border-radius: 10px;
+  }
 `;
 
 const ButtonContainer = styled.div`
@@ -123,75 +176,14 @@ const IconButton = styled.div`
   background-color: var(--color-sub-2);
   cursor: pointer;
 
-  svg{
+  svg {
     width: 12px;
     height: 11px;
     fill: var(--color-sub-3);
-    :hover{
+    :hover {
       fill: var(--color-main-4);
     }
   }
-`;
-
-const URLButton = styled.button`
-  max-width: 120px;
-  max-height: 120px;
-  min-height: 65px;
-  min-width: 30px;
-  flex: 0 0 calc(30% - 20px);
-  padding: 25px;
-  border-radius: 10px;
-  color: var(--color-sub-3);
-  background-color: var(--color-sub-4);
-  font-size: 0.93rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.3));
-  white-space: pre-line;
-  position: relative;
-`;
-
-const ArrowUp = styled.div`
-  position: absolute;
-  top: 10%;
-  right: 5%;
-  svg{
-    height: 24px;
-    width: 24px;
-    fill: var(--color-sub-1);
-  }
-`;
-
-const LinkContainer = styled.div`
-  position: absolute;
-  bottom: 10%;
-  left: 50%;
-  transform: translateX(-50%);
-  color: var(--color-sub-1);
-  font-size: 15px;
-  display: flex;
-  flex-direction: row;
-  white-space: nowrap;
-  svg{
-    width: auto;
-    height: 18px;
-    fill: var(--color-sub-1);
-    margin-right: 5px;
-  }
-`;
-
-const URLButtonContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  justify-content: center;
-  align-items: flex-end;
-  gap: 10px;
-  padding: 35px 10px;
-  box-sizing: border-box;
 `;
 
 const ProjectMemberContainer = styled.div`
@@ -199,19 +191,27 @@ const ProjectMemberContainer = styled.div`
   flex: 1;
   width: 100%;
   justify-content: center;
+  @media (max-width: 768px) {
+    grid-area: team;
+  }
+`;
+
+const ProjectLinksContainer = styled.div`
+  width: 100%;
+  @media (max-width: 768px) {
+    grid-area: link;
+  }
 `;
 
 export const ProjectBoard = () => {
-  const [isInitial , setIsInitial] = useState(false);
-  const [proejctLink, setProjectLinks] = useState(
-    {
-      github: "github.com",
-      notion: "notion.com",
-      link1: "link1",
-      link2: null
-    }
-    )
-  
+  const [isInitial, setIsInitial] = useState(false);
+  const [proejctLink, setProjectLinks] = useState({
+    github: "github.com",
+    notion: "notion.com",
+    link1: "link1",
+    link2: null
+  });
+
   const onClickEditButton = () => {
     //go to edit page
   };
@@ -220,88 +220,75 @@ export const ProjectBoard = () => {
       username: "user1",
       image: "",
       part: "안드로이드"
-    },{
+    },
+    {
       username: "user2",
       image: "",
       part: "서버"
     }
-  ]
+  ];
 
   return (
-    <Wrapper>
-      <BackContainer>
-        <GoBack />
-      </BackContainer>
-      <TeamContainer>
-        <ProjectMemberContainer>
-        <ProjectMembers 
-        team={teamMember}
-        UserButton={<IconButton><AiFillHome/></IconButton>}
-        OthersButton={
-          <ButtonContainer>
-          <IconButton><AiFillHome/></IconButton>
-          <IconButton><BsFillSendFill/></IconButton>
-        </ButtonContainer>
-        }
-        />
-        </ProjectMemberContainer>
-        {isInitial ?
-        <URLButtonContainer>
-          <URLButton>
-            URL을{"\n"}추가하세요
-          </URLButton>
+    <Container>
+      <Wrapper>
+        <BackContainer>
+          <GoBack />
+        </BackContainer>
 
-          <URLButton>
-            URL을{"\n"}추가하세요
-          </URLButton>
-        </URLButtonContainer>
-      :
-      <URLButtonContainer>
-        <URLButton>
-         <ArrowUp><BsArrowUpRight/></ArrowUp>
-         <LinkContainer><BsGithub />깃허브</LinkContainer>
-        </URLButton>
+        <TeamContainer>
+          <ProjectMemberContainer>
+            <ProjectMembers
+              team={teamMember}
+              UserButton={
+                <IconButton>
+                  <AiFillHome />
+                </IconButton>
+              }
+              OthersButton={
+                <ButtonContainer>
+                  <IconButton>
+                    <AiFillHome />
+                  </IconButton>
+                  <IconButton>
+                    <BsFillSendFill />
+                  </IconButton>
+                </ButtonContainer>
+              }
+            />
+          </ProjectMemberContainer>
+          <ProjectLinksContainer>
+            <ProjectLinks />
+          </ProjectLinksContainer>
+        </TeamContainer>
 
-        <URLButton>
-        <ArrowUp><BsArrowUpRight /></ArrowUp>
-         <LinkContainer><SiNotion />팀 노션</LinkContainer>
-        </URLButton>
+        <TitleContainer>
+          커리어 SNS 기반 멘토링 플랫폼 프로젝트{" "}
+          <AiFillSetting onClick={onClickEditButton} />
+        </TitleContainer>
 
-        <URLButton>
-        <ArrowUp><BsArrowUpRight /></ArrowUp>
-         <LinkContainer><BiLinkAlt />참고 링크</LinkContainer>
-        </URLButton>
-        <URLButton>
-        <ArrowUp><BsArrowUpRight /></ArrowUp>
-         <LinkContainer><BiLinkAlt />참고 링크</LinkContainer>
-        </URLButton>
-    </URLButtonContainer>
-      }
-      </TeamContainer>
+        <DdayContainer>
+          <ProjectDday />
+        </DdayContainer>
+        <MiniDdayContainer>
+          <MiniDday />
+        </MiniDdayContainer>
 
-      <TitleContainer>
-        커리어 SNS 기반 멘토링 플랫폼 프로젝트{" "}
-        <AiFillSetting onClick={onClickEditButton} />
-      </TitleContainer>
-
-      <DdayContainer>
-        <ProjectDday />
-      </DdayContainer>
-      <LangContainer>
-        <Language />
-      </LangContainer>
-      <CommitLevelContainer>
-        <CommitLevel />
-      </CommitLevelContainer>
-      <ChatContainer>
-        <ProjectChatBox />
-      </ChatContainer>
-      <CommitGraphContainer>
-        <CommitGraph type="month" />
-      </CommitGraphContainer>
-      <TodayCommitContainer>
-        <CommitGraph type="today" />
-      </TodayCommitContainer>
-    </Wrapper>
+        <LangContainer>
+          <Language />
+        </LangContainer>
+        <CommitLevelContainer>
+          <CommitLevel />
+        </CommitLevelContainer>
+        <ChatContainer>
+          <ProjectChatBox />
+        </ChatContainer>
+        <CommitGraphContainer>
+          <CommitGraph type="month" />
+        </CommitGraphContainer>
+        <TodayCommitContainer>
+          <CommitGraph type="today" />
+        </TodayCommitContainer>
+      </Wrapper>
+    </Container>
   );
 };
