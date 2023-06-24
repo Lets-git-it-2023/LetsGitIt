c2587b15cfc0e3b559d4a9d581126a554f6bfaf5
ehgmlKim <dohee930@gmail.com>
2023-06-24 12:22:40 +0900

    Rename: Comment -> CommunityComment로 수정

--- src/main/java/com/proj/letsgitit/entity/CommunityComment.java
+++ src/main/java/com/proj/letsgitit/entity/CommunityComment.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @Table(name="comment")
-public class Comment {
+public class CommunityComment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="comment_id")
