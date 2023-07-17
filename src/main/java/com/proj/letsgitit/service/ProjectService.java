20ef8c0c946e4e12b3c97742fb11ebfe533a6ea0
ehgmlKim <dohee930@gmail.com>
2023-07-18 01:11:47 +0900

    Feat: 전체 프로젝트 게시판 조회
    검색 구현, 최신순, 조회수순 정렬 구현

--- src/main/java/com/proj/letsgitit/service/ProjectService.java
+++ src/main/java/com/proj/letsgitit/service/ProjectService.java
@@ -10,6 +10,10 @@ import com.proj.letsgitit.repository.UserProjectRepository;
 import com.proj.letsgitit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -24,6 +28,26 @@ public class ProjectService {
     private final ProjectRepository projectRepository;
     private final UserProjectRepository userProjectRepository;
     private final UserRepository userRepository;
+
+    // 전체 프로젝트 게시글 조회
+    public Page<Project> findAll(String title, String content, int page, int sortType) {
+        Pageable pageable = null;
+        switch (sortType){
+            case 0: // 최신순
+                pageable = PageRequest.of(page, 16, Sort.by("id").ascending());
+                break;
+//            case 1: // 목표 도달률순
+//                pageable = PageRequest.of(page, 16, Sort.by("countComment").descending());
+//                break;
+            case 2: // 조회수순
+                pageable = PageRequest.of(page, 16, Sort.by("countVisit").descending());
+//                break;
+//            case 3: // 커밋순
+//                pageable = PageRequest.of(page, 16, Sort.by("countVisit").descending());
+//                break;
+        }
+        return projectRepository.findByTitleContainingOrContentContaining(title, content, pageable);
+    }
 
     // 프로젝트 생성
     public Long create(ProjectCreateDto dto) {
@@ -56,6 +80,9 @@ public class ProjectService {
     public Project findById(Long id) {
         Project project = projectRepository.findById(id).orElseThrow((() ->
                 new IllegalStateException("해당 프로젝트가 존재하지 않습니다.")));
+
+        int cnt = project.getCountVisit() + 1;
+        project.updateVisit(cnt);
         return project;
     }
 
