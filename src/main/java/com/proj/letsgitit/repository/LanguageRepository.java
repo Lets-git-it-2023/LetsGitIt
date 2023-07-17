0b9d50d92330ea719fb4c639060ef23e268f9a3d
ehgmlKim <dohee930@gmail.com>
2023-07-17 22:48:55 +0900

    Remove: 사용언어, 툴 추가/수정 방식 변경에 따라 이전 방식의 controller, service, dto, entity, repository 삭제

--- src/main/java/com/proj/letsgitit/repository/LanguageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proj.letsgitit.repository;
-
-import com.proj.letsgitit.entity.Language;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LanguageRepository extends JpaRepository<Language, Long> {
-}
