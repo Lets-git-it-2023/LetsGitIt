890080c6b7d74c7f3d2b380357c0fabe36366eb8
ehgmlKim <dohee930@gmail.com>
2023-07-18 02:03:25 +0900

    Feat: 프로젝트 채팅 entity 생성

---
+++ src/main/java/com/proj/letsgitit/entity/ProjectChat.java
@@ -0,0 +1,31 @@
+package com.proj.letsgitit.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectChat extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="project_chat_id")
+    private Long id;
+    private String content;
+    private String createdBy; // 댓글 작성자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id") // 외래키를 설정
+    @JsonBackReference
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id") // 외래키를 설정
+    @JsonBackReference
+    private Project project;
+}
